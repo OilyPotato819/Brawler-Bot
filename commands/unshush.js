e6842d278264b056b9da6381e64bb6b62a2dedf1
OilyPotato819 <o.schneider@share.epsb.ca>
2022-05-01 01:08:41 -0600

    created unshush

---
+++ commands/unshush.js
@@ -0,0 +1,7 @@
+module.exports = {
+   name: 'greeting',
+   description: 'greet author of message',
+   async execute(message, args) {
+      message.reply(`Hello ${message.author.username}`);
+   },
+};

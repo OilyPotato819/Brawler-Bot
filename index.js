d5ffded21d8549077abc99541ad6ea3fc7263b2c
OilyPotato819 <o.schneider@share.epsb.ca>
2023-01-31 22:21:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { deployCommands } = require('./deploy-commands');
 deployCommands();
 
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
@@ -50,6 +51,30 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 client.on('ready', () => {
    console.log(`Logged in as ${client.user.tag}!`);
+
+   let results = ['1', '2', '3', '4', '5'];
+   let rows = [];
+
+   for (let i = 0; i < results.length; i++) {
+      rows.push(
+         new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+               .setCustomId(i.toString())
+               .setLabel((i + 1).toString())
+               .setStyle(ButtonStyle.Primary)
+         )
+      );
+   }
+
+   let channel = client.channels.cache.get('964752876306055168');
+
+   const actionRow = new ActionRowBuilder();
+
+   actionRow.addComponents(new ButtonBuilder().setCustomId('back').setStyle(ButtonStyle.Primary).setEmoji('⏮'));
+   actionRow.addComponents(new ButtonBuilder().setCustomId('play').setStyle(ButtonStyle.Primary).setEmoji('▶'));
+   actionRow.addComponents(new ButtonBuilder().setCustomId('forward').setStyle(ButtonStyle.Primary).setEmoji('⏭'));
+
+   channel.send({ content: 'https://www.youtube.com/watch?v=p0BkfX2Twdc', components: [actionRow] });
 });
 
 client.login(token);

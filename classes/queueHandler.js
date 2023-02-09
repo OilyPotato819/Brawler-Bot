707586fbc8d2f7158a41880931fd2ed89470e80a
OilyPotato819 <o.schneider@share.epsb.ca>
2023-02-09 21:49:06 +0000

    Update 5 files

--- classes/queueHandler.js
+++ classes/queueHandler.js
@@ -1,29 +1,38 @@
-const { Queue } = require('./queue.js');
+const { Queue } = require("./queue.js");
 
 module.exports = {
-   QueueHandler: class {
-      constructor() {
-         this.queues = new Map();
+  QueueHandler: class {
+    constructor() {
+      this.queues = new Map();
+    }
+
+    getQueue(interaction) {
+      const guildId = interaction.guildId;
+      const vc = interaction.member.voice.channel;
+      const client = interaction.client;
+
+      if (!vc) {
+        const message = {
+          content: `you must be in a vc to add to queue`,
+          components: [],
+          embeds: [],
+          ephemeral: true,
+        };
+
+        if (interaction.replied) {
+          interaction.editReply(message);
+        } else {
+          interaction.reply(message);
+        }
+
+        return;
       }
 
-      getQueue(interaction) {
-         const guildId = interaction.guildId;
-         const vc = interaction.member.voice.channel;
-         const client = interaction.client;
-
-         if (!vc) {
-            interaction.editReply({
-               content: `you must be in a vc to add to queue`,
-               components: [],
-               embeds: [],
-            });
-         }
-
-         if (!this.queues.get(guildId)) {
-            this.queues.set(guildId, new Queue(vc, client));
-         }
-
-         return this.queues.get(guildId);
+      if (!this.queues.get(guildId)) {
+        this.queues.set(guildId, new Queue(vc, client));
       }
-   },
+
+      return this.queues.get(guildId);
+    }
+  },
 };
--- commands/skip.js
+++ commands/skip.js
@@ -1,23 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js");
+
 module.exports = {
-   data: new SlashCommandBuilder()
-      .setName('play')
-      .setDescription('play a song')
-      .addStringOption((option) => option.setName('input').setDescription('link or song name').setRequired(true)),
-   async execute(interaction) {
-      const input = interaction.options.getString('input');
-      const queue = interaction.client.queueHandler.getQueue(interaction);
+  data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("skip the first song in queue"),
+  async execute(interaction) {
+    const queue = interaction.client.queueHandler.getQueue(interaction);
 
-      if (!interaction.member.voice.channel) return interaction.reply({ content: 'you must be in a vc to use this command', ephemeral: true });
+    if (!queue) return;
 
-      if (input.startsWith('https://')) {
-         await axios.get('https://www.youtube.com/oembed?format=json&url=' + input).catch(() => {
-            return interaction.reply({ content: 'not a valid link', ephemeral: true });
-         });
-         queue.add(input);
-         interaction.reply(`added ${input} to queue`);
-      } else {
-         const youtubeResults = await queue.search(input);
-         interaction.songMenu = new SongMenu(interaction, youtubeResults);
-      }
-   },
+    queue.skip();
+  },
 };

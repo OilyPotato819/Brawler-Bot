e7d749d3e480dc3efbbc263dd5094ff257f0e72c
OilyPotato819 <o.schneider@share.epsb.ca>
2023-02-13 20:43:57 +0000

    Update 3 files

---
+++ commands/pause.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pause")
+    .setDescription("pause the current song"),
+  async execute(interaction) {
+    const queue = interaction.client.queueHandler.getQueue(interaction);
+
+    if (!queue) return;
+
+    queue.pause(interaction);
+  },
+};
